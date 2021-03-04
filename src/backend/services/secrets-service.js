b8a803ca796a3177d147a3f20a3f58106352dc65
Karolis Astrauka <karolisa@wix.com>
2021-03-04 09:59:38 +0200

    Log on secrets fetch

--- src/backend/services/secrets-service.js
+++ src/backend/services/secrets-service.js
@@ -1,15 +1,18 @@
+import { withLogger } from '../utils/logger';
 export class SecretsService {
     constructor(externals) {
         this.externals = externals;
     }
     async getSecrets() {
-        const [passwordSalt, passwordSecret] = await Promise.all([
-            this.externals.getSecret('PASSWORD_SALT'),
-            this.externals.getSecret('PASSWORD_SECRET'),
-        ]);
-        return {
-            passwordSalt,
-            passwordSecret,
-        };
+        return withLogger('getSecrets', async () => {
+            const [passwordSalt, passwordSecret] = await Promise.all([
+                this.externals.getSecret('PASSWORD_SALT'),
+                this.externals.getSecret('PASSWORD_SECRET'),
+            ]);
+            return {
+                passwordSalt,
+                passwordSecret,
+            };
+        });
     }
 }
--- typescript/backend/services/secrets-service.ts
+++ typescript/backend/services/secrets-service.ts
@@ -1,16 +1,19 @@
 import { Externals } from '../context/production-context';
+import { withLogger } from '../utils/logger';
 
 export class SecretsService {
   constructor(private readonly externals: Externals) {}
 
   public async getSecrets() {
-    const [passwordSalt, passwordSecret] = await Promise.all([
-      this.externals.getSecret('PASSWORD_SALT'),
-      this.externals.getSecret('PASSWORD_SECRET'),
-    ]);
-    return {
-      passwordSalt,
-      passwordSecret,
-    };
+    return withLogger('getSecrets', async () => {
+      const [passwordSalt, passwordSecret] = await Promise.all([
+        this.externals.getSecret('PASSWORD_SALT'),
+        this.externals.getSecret('PASSWORD_SECRET'),
+      ]);
+      return {
+        passwordSalt,
+        passwordSecret,
+      };
+    });
   }
 }
