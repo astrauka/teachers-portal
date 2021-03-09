ac5471ce9ea8c0e13e154566be3c9111f426dfb6
Karolis Astrauka <karolisa@wix.com>
2021-03-09 21:04:59 +0200

    Rename TeacherProfile to Teachers

---
+++ src/pages/Teachers_(Slug).ooejw/Teachers_(Slug).js
@@ -0,0 +1,86 @@
+import { forEach } from 'lodash';
+import { AccountStatuses, AccountStatusIds } from 'public/common/entities/teacher';
+import { forCurrentTeacher } from 'public/for-current-teacher';
+import { ImageDefault, setImageDefault } from 'public/images';
+import wixLocation from 'wix-location';
+const SOCIAL_ICONS = {
+    facebook: 'https://www.facebook.com/',
+    instagram: 'https://www.instagram.com/',
+    linkedIn: 'https://www.linkedin.com/in/',
+};
+forCurrentTeacher('teachersProfile', async () => {
+    $w('#SelectedTeacher').onReady(() => {
+        const teacher = $w('#SelectedTeacher').getCurrentItem();
+        setImageDefault(teacher.profileImage, $w('#profileImage'), ImageDefault.Profile);
+        addSocialIconLinks(teacher);
+        addWebsiteLink(teacher);
+        addSendEmailButton(teacher);
+        addAboutHtml(teacher);
+        addTeachingModules(teacher);
+        showFilledInformation(teacher);
+        showStatus(teacher);
+    });
+});
+function addSocialIconLinks(teacher) {
+    forEach(SOCIAL_ICONS, (url, provider) => {
+        const link = teacher[provider];
+        const $icon = $w(`#${provider}`);
+        if (link) {
+            $icon.target = '_blank';
+            $icon.link = `${url}${link}`;
+            $icon.expand();
+        }
+    });
+}
+function addWebsiteLink(teacher) {
+    const $website = $w('#website');
+    const { website } = teacher;
+    if (website) {
+        $website.html = `<a href="${website}" target="_blank">${website}</a>`;
+        $website.expand();
+    }
+}
+function addSendEmailButton(teacher) {
+    const $button = $w('#curatingTeacherAskButton');
+    const { email } = teacher;
+    $button.onClick(() => {
+        wixLocation.to(`mailto:${email}?subject=MRY%3A%20Question`);
+    });
+}
+function addAboutHtml(teacher) {
+    if (teacher.about) {
+        $w('#about').html = teacher.about;
+        $w('#aboutGroup').expand();
+    }
+}
+function showFilledInformation(teacher) {
+    var _a;
+    if (teacher.countryId) {
+        $w('#country').expand();
+    }
+    if (teacher.city) {
+        $w('#city').expand();
+    }
+    if ((_a = teacher.photos) === null || _a === void 0 ? void 0 : _a.length) {
+        $w('#photosGroup').expand();
+    }
+    const $menteesDataset = $w('#MenteesDataset');
+    $menteesDataset.onReady(() => {
+        if ($menteesDataset.getTotalCount()) {
+            $w('#menteesGroup').expand();
+        }
+    });
+}
+function showStatus(teacher) {
+    var _a;
+    if (teacher.statusId === AccountStatusIds.Active ||
+        ((_a = teacher.statusId) === null || _a === void 0 ? void 0 : _a.title) === AccountStatuses.Active) {
+        $w('#teachersStatusActive').expand();
+    }
+    else {
+        $w('#teachersStatusInactive').expand();
+    }
+}
+function addTeachingModules(teacher) {
+    $w('#modules').text = teacher.modules ? `Teaching modules: ${teacher.modules}` : '';
+}
--- src/public/common/entities/teacher.js
+++ src/public/common/entities/teacher.js
@@ -11,6 +11,12 @@ export var AccountStatuses;
     AccountStatuses["Inactive"] = "Inactive";
     AccountStatuses["NotATeacher"] = "Not a teacher";
 })(AccountStatuses || (AccountStatuses = {}));
+export var AccountStatusIds;
+(function (AccountStatusIds) {
+    AccountStatusIds["Active"] = "01dbd966-7e18-43ea-8326-a1a6d15673a6";
+    AccountStatusIds["Inactive"] = "ef855569-356d-47da-8f91-09df8c9570af";
+    AccountStatusIds["NotATeacher"] = "f43d07b7-28da-4097-9541-a18d4ec1fc76";
+})(AccountStatusIds || (AccountStatusIds = {}));
 export var TaskName;
 (function (TaskName) {
     TaskName["initialProfileForm"] = "initialProfileForm";
