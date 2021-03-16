ea4776547e5ab70e6d60f3fe3b49a40002639d8b
Karolis Astrauka <karolisa@wix.com>
2021-03-16 09:43:34 +0200

    rsync files to keep editor alive

--- scripts/configure-frontend.ts
+++ scripts/configure-frontend.ts
@@ -3,40 +3,40 @@ import * as fs from 'fs';
 import { difference } from 'lodash';
 import * as path from 'path';
 import * as copy from 'recursive-copy/lib/copy';
+import { rsync } from './rsync';
 
 const CURRENT_DIR = path.resolve(__dirname, './');
 const DIST_DIR = path.resolve(CURRENT_DIR, '../dist');
-const SRC_DIR = path.resolve(CURRENT_DIR, '../src');
+const SRC_DIR = path.resolve(DIST_DIR, './src');
 
 async function configureFrontendPublic() {
   const PUBLIC_DIST_DIR = path.resolve(DIST_DIR, './frontend/public');
   const PUBLIC_SRC_DIR = path.resolve(SRC_DIR, './public');
 
   await del(PUBLIC_SRC_DIR);
-  await copy(PUBLIC_DIST_DIR, PUBLIC_SRC_DIR, {filter: ['**/*.js', '!**/*.spec.js']});
-  fs.copyFileSync(`${CURRENT_DIR}/tsconfig.public.json`, `${PUBLIC_SRC_DIR}/tsconfig.json`);
+  await copy(PUBLIC_DIST_DIR, PUBLIC_SRC_DIR, { filter: ['**/*.js', '!**/*.spec.js'] });
+  await copy(`${CURRENT_DIR}/tsconfig.public.json`, `${PUBLIC_SRC_DIR}/tsconfig.json`);
+  await rsync(`${PUBLIC_SRC_DIR}/`, path.resolve(CURRENT_DIR, '../src/public'));
 }
 
 async function configureFrontendPages() {
   const PAGES_DIST_DIR = path.resolve(DIST_DIR, './frontend/pages');
   const PAGES_SRC_DIR = path.resolve(SRC_DIR, './pages');
-  await configurePages(PAGES_DIST_DIR, PAGES_SRC_DIR);
+  const WIX_PAGES_SRC_DIR = path.resolve(CURRENT_DIR, '../src/pages');
+  await del(PAGES_SRC_DIR);
+  await configurePages(PAGES_DIST_DIR, PAGES_SRC_DIR, WIX_PAGES_SRC_DIR);
+  await rsync(`${PAGES_SRC_DIR}/`, WIX_PAGES_SRC_DIR, { override: false });
 }
 
-async function configureFrontendLightboxes() {
-  const PAGES_DIST_DIR = path.resolve(DIST_DIR, './frontend/lightboxes');
-  const PAGES_SRC_DIR = path.resolve(SRC_DIR, './lightboxes');
-  await configurePages(PAGES_DIST_DIR, PAGES_SRC_DIR);
-}
-
-async function configurePages(distDir: string, srcDir: string) {
-  const typescriptPages = fs.readdirSync(distDir)
-    .filter(file => !file.endsWith('.spec.js'))
-    .map(file => file.split('.js')[0]);
-  const corvidPagesDirectories =
-    fs.readdirSync(srcDir, {withFileTypes: true})
-      .filter((file) => file.isDirectory())
-      .map((directory) => directory.name);
+async function configurePages(distDir: string, srcDir: string, wixSrcDir: string) {
+  const typescriptPages = fs
+    .readdirSync(distDir)
+    .filter((file) => !file.endsWith('.spec.js'))
+    .map((file) => file.split('.js')[0]);
+  const corvidPagesDirectories = fs
+    .readdirSync(wixSrcDir, { withFileTypes: true })
+    .filter((file) => file.isDirectory())
+    .map((directory) => directory.name);
 
   const pagesToCopy = corvidPagesDirectories
     .map((directory) => {
@@ -50,19 +50,16 @@ async function configurePages(distDir: string, srcDir: string) {
   if (typescriptPages.length !== pagesToCopy.length) {
     const missingPages = difference(
       typescriptPages,
-      pagesToCopy.map(([page]) => page),
+      pagesToCopy.map(([page]) => page)
     );
     throw new Error(`Typescript pages did not match source pages: ${missingPages}`);
   }
 
-  pagesToCopy.forEach(([page, directory]) => {
-    fs.copyFileSync(`${distDir}/${page}.js`, `${srcDir}/${directory}/${page}.js`);
-  });
-
+  await Promise.all(
+    pagesToCopy.map(([page, directory]) =>
+      copy(`${distDir}/${page}.js`, `${srcDir}/${directory}/${page}.js`)
+    )
+  );
 }
 
-Promise.all([
-  configureFrontendPublic(),
-  configureFrontendPages(),
-  // configureFrontendLightboxes(), unused
-]).then(() => console.log('Done'));
+Promise.all([configureFrontendPublic(), configureFrontendPages()]).then(() => console.log('Done'));
